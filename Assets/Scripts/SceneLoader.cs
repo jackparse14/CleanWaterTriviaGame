0fac127ccda31a8d4f73ce4ebea2fe43c14e1b35
Jack Parsons <65678430+jackparse14@users.noreply.github.com>
2021-08-04 17:11:16 +0100

    More Scenes

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void LoadAnswerScene()
+    {
+        SceneManager.LoadScene("AnswerScene");
+    }
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenuScene");
+    }
+    public void LoadLearnScene()
+    {
+        SceneManager.LoadScene("LearnScene");
+    }
+    public void LoadOptionsScene()
+    {
+        SceneManager.LoadScene("OptionsScene");
+    }
+}
